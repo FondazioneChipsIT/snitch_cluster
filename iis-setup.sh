bb2d2f754b2949e756919df84551e71560d2016c
Opter-One <luca.colombo07@universitadipavia.it>
2025-12-09 15:10:57 +0100

    Setup modifications

--- iis-setup.sh
+++ iis-setup.sh
@@ -4,24 +4,25 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # Define environment variables
-export CC=gcc-9.2.0
-export CXX=g++-9.2.0
-export SN_BENDER=bender-0.28.1
-export SN_VCS_SEPP=vcs-2024.09
-export SN_VERILATOR_SEPP=oseda
-export SN_QUESTA_SEPP=questa-2023.4
-export SN_LLVM_BINROOT=/usr/scratch2/vulcano/colluca/tools/riscv32-snitch-llvm-almalinux8-15.0.0-snitch-0.2.0/bin
+export CC=
+export CXX=
+export SN_BENDER=bender
+export SN_VCS_SEPP=
+export SN_VERILATOR_SEPP=
+export SN_QUESTA_SEPP=
+export SN_LLVM_BINROOT=/opt/riscv/snitch-llvm-15.0.0-snitch-0.2.0/bin
 
 # Create Python virtual environment with required packages
-/usr/local/anaconda3-2023.07/bin/python -m venv .venv
-source .venv/bin/activate
+#python3.11 -m venv .venv
+#source .venv/bin/activate
+#pip install -r requirements.txt
 # Install local packages in editable mode and unpack packages in a
 # local temporary directory which can be safely cleaned after installation.
 # Also protects against "No space left on device" errors
 # occurring when the /tmp folder is filled by other processes.
-mkdir tmp
-TMPDIR=tmp pip install -e .[all]
-rm -rf tmp
+#mkdir tmp
+#TMPDIR=tmp pip install -e .[all]
+#rm -rf tmp
 
 # Add simulator binaries to PATH
 export PATH=$PWD/target/sim/build/bin:$PATH
