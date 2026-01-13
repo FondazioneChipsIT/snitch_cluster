8128047b84cb7b27b74cb333a8c0b9f6b7f26dbb
Opter-One <luca.colombo07@universitadipavia.it>
2026-01-13 14:51:53 +0100

    Some performance improv. in DWT, or so i think

--- sw/benchmarks/DSP/DWT/data/data.h
+++ sw/benchmarks/DSP/DWT/data/data.h
@@ -5,7 +5,7 @@ double *x,*y_low, *y_high,*h,*g;
 
 // Vector length (for now only a multiple of ncores, default 8)
 #ifndef LEN
-#define LEN 2048
+#define LEN 16
 #endif
 // If you exceed TCDM size you get 0 as result
 // Max size is 128KB/8bytes (64bits for doubles) = 16k theoretically -> 8K theoretical for each vector!
