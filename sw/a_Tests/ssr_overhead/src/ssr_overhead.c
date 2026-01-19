5dd47bc611d3d9b5ff2d950f9679e2a7fed89b6c
Opter-One <luca.colombo07@universitadipavia.it>
2026-01-19 11:01:05 +0100

    To use fdiv, use div.json

--- sw/a_Tests/ssr_overhead/src/ssr_overhead.c
+++ sw/a_Tests/ssr_overhead/src/ssr_overhead.c
@@ -10,27 +10,32 @@ int main() {
     uint32_t m =2;
     uint32_t x =7;
     if(core_idx == 0) {
+
+
         uint64_t start = snrt_mcycle();
 
-
+        asm volatile ("START:");
         // 3 SSR overheads
-        snrt_ssr_loop_1d(SNRT_SSR_DM0, 64, sizeof(double));
-        snrt_ssr_loop_1d(SNRT_SSR_DM1, 64, sizeof(double));
-        snrt_ssr_loop_1d(SNRT_SSR_DM2, 64, sizeof(double));
+        snrt_ssr_loop_4d(SNRT_SSR_DM0, 64, 64, 64, 64, sizeof(double), sizeof(double), sizeof(double), sizeof(double));
+        /*snrt_ssr_loop_1d(SNRT_SSR_DM1, 64, sizeof(double));
+        snrt_ssr_loop_1d(SNRT_SSR_DM2, 64, sizeof(double));*/
 
-        snrt_ssr_read(SNRT_SSR_DM0, SNRT_SSR_1D, test_pointer + offset*x+m);
-        snrt_ssr_read(SNRT_SSR_DM1, SNRT_SSR_1D, test_pointer +  offset*m+x); 
-        snrt_ssr_write(SNRT_SSR_DM2, SNRT_SSR_1D, test_pointer +  m*x+offset); 
+        snrt_ssr_read(SNRT_SSR_DM0, SNRT_SSR_4D, test_pointer + offset*x+m);
+        //snrt_ssr_read(SNRT_SSR_DM1, SNRT_SSR_1D, test_pointer +  offset*m+x);
+        //snrt_ssr_write(SNRT_SSR_DM2, SNRT_SSR_1D, test_pointer +  m*x+offset); 
         
         snrt_ssr_enable();
 
 
         snrt_ssr_disable();
-        snrt_fpu_fence();
+        
+        asm volatile ("END:");
 
         uint64_t end = snrt_mcycle();
 
         uint64_t cycles = end - start;
+
+        snrt_fpu_fence();
 
         printf("SSR Overhead Cycles: %llu\n", cycles);
     }
