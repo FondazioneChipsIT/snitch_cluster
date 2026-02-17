94fffd93cd8594a0623337ed1faefbeb64de4503
Opter-One <luca.colombo07@universitadipavia.it>
2026-02-17 14:28:30 +0100

    [HW] Freeze RTL-Cluster Synthesis

---
+++ hw/generated/bootdata.cc
@@ -0,0 +1,20 @@
+// Copyright 2021 ETH Zurich and University of Bologna.
+// Solderpad Hardware License, Version 0.51, see LICENSE for details.
+// SPDX-License-Identifier: SHL-0.51
+
+#include <tb_lib.hh>
+
+namespace sim {
+
+const BootData BOOTDATA = {.boot_addr = 0x80000000,
+                           .core_count = 9,
+                           .hartid_base = 0,
+                           .tcdm_start = 0x10000000,
+                           .tcdm_size = 0x20000,
+                           .tcdm_offset = 0x0,
+                           .global_mem_start = 0x80000000,
+                           .global_mem_end = 0x100000000,
+                           .cluster_count = 1,
+                           .clint_base = 0xffff1000};
+
+}  // namespace sim
